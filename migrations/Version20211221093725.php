7078abd81baebfd5c9cce05fff465a25dd031142
Clovis <gerin.patrice@yahoo.fr>
2021-12-31 17:24:00 +0100

    add a cart

---
+++ migrations/Version20211221093725.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211221093725 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, soc_gest VARCHAR(255) NOT NULL, categorie VARCHAR(255) NOT NULL, capital VARCHAR(255) NOT NULL, thematique VARCHAR(255) NOT NULL, cretated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', capitalisation VARCHAR(255) NOT NULL, nb_assoc INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE produit');
+    }
+}
---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProduitController extends AbstractController
+{
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/produits", name="produits")
+     */
+    public function index(ProduitRepository $repo): Response
+    {
+        $produits = $repo->findAll();
+        return $this->render('produit/index.html.twig', compact('ProduitController'),
+        );
+    }
+}
