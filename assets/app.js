4cf5091636c1f1014e4067e7db2cd4d1448207f0
Clovis <gerin.patrice@yahoo.fr>
2022-01-06 18:37:45 +0100

    adding liip

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,13 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import $ from 'jquery';
 
 // start the Stimulus application
 import './bootstrap';
+
+
+$('.custom-file-input').on('change', function (e) {
+    var inputFile = e.currentTarget;
+   $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+});
